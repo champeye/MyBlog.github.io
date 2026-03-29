51f04f770d8bede979518c027e739f388aa01434
champeye <muhammadshanawer1@gmail.com>
2026-03-30 00:18:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+// Filhaal isko khali chhod sakte hain ya koi welcome message daal sakte hain
+console.log("ChampyEye Blog Loaded Successfully!");
 
+// Future mein yahan search feature ya filters daal sakte hain
